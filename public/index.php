4ea92dfd8b9667397209cdc632aa33ffb8e2d041
Savanah Steigen <savanahsteigen@gmail.com>
2016-10-08 16:10:50 -0400

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,244 @@
+<html class="no-js" lang="en" >
+<head>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>The Yarn Basket</title>
+	
+	<link rel="stylesheet" href="css/normalize.css">
+  	<link rel="stylesheet" href="css/foundation.css">
+
+	<link href='http://fonts.googleapis.com/css?family=Josefin+Slab:400,600,700,400italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/app.css">
+
+	<script src="js/vendor/modernizr.js"></script>
+</head>
+<body>
+
+	<!-- start header  -->
+	<header>
+		<div>
+			<h1>The Yarn Basket</h1>
+		</div>
+	</header>
+	<!-- end header -->
+
+		<div class="page-wrapper small-10 small-centered columns">
+			<!-- start page wrapper row -->
+			<div class=" row">
+				<div class="small-12 columns">
+					<!-- start sidebar and main page content -->
+					<div class="row">
+						<!-- start sidebar content -->
+						<div class="sidebar small-12 medium-5 large-3 columns">
+							<ul class="side-nav">
+								<li><a href="#home">Home</a></li>
+								<li><a href="#about-us">About Us</a></li>
+								<li><a href="#contact-us">Contact Us</a></li>
+								<li><a href="#products">Products</a></li>
+								<li><a href="#classes">Classes</a></li>
+								<li><a href="#sale">Sale</a></li>
+							</ul>
+							<form>
+								<div class="row">
+									<div class="small-12 columns">
+									<h3>Subscribe</h3>
+									<p>Sign up to receive emails about future sales and offers from <span class="brand">The Yarn Basket</span>!</p>
+										<input type="text" placeholder="Email Address">
+										<a href="#" class="button small expand radius">Subscribe</a>
+									</div>
+								</div>
+							</form>
+							<a href="https://www.facebook.com/pages/The-Yarn-Basket/158373450863860" target="_blank" class="facebook button small expand radius">Check us out on Facebook!</a>
+						</div>
+						<!-- end sidebar content -->
+						<!-- start main page content -->
+						<div class="small-12 medium-7 large-9 columns">
+							<!-- start welcome section row -->
+							<div class="row">
+								<div class="small-12 columns">
+									<h1 id="home">Welcome</h1>
+								</div>
+								<!-- start welcome section left -->
+								<div class="small-12 medium-12 large-6 columns">
+									<p>Just off of Route 30 in Chambersburg lies a wonderful haven for knitters and needlework enthusiasts.  Please stop by to see how we can best serve you!</p>	
+									<ul class="small-block-grid-1">
+										<li><img class="th" src="http://placekitten.com/400/246" /></li>
+									</ul>
+								</div>
+								<!-- end welcome section left -->
+								<!-- start welcome section right -->
+								<div class="small-12 medium-12 large-6 columns">
+									<div class="stitched">
+										<div class="row">
+											<div class="small-12 columns">
+												<h3>Hours</h3>
+											</div>
+											<div class="small-5 columns">
+												<p>Mon</p>
+												<p>Tues</p>
+												<p>Wed</p>
+												<p>Thurs</p>
+												<p>Fri</p>
+												<p>Sat</p>
+												<p>Sun</p>
+											</div>
+											<div class="small-7 columns">
+												<p>10am - 5pm</p>
+												<p>10am - 5pm</p>
+												<p>10am - 5pm</p>
+												<p>Closed</p>
+												<p>10am - 5pm</p>
+												<p>10am - 3pm</p>
+												<p>Closed</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!-- end welcome section right -->
+							</div>
+							<!-- end welcome section row -->
+							<!-- start about us section row -->
+							<div class="row">
+								<div class="small-12 columns">
+									<h1 id="about-us">About Us</h1>
+								</div>
+								<div class="small-12 medium-12 large-6 columns">
+									<p>Nestled in the heart of the Cumberland Valley lies a wonderful haven for knitters, and needlework enthusiasts. <span class="brand">The Yarn Basket</span> is Central Pennsylvania's Home for knitting and needlework. With our large selection of yarns, you can very easily make this your only stop for your needlework supplies.</p>
+								</div>
+								<div class="small-12 medium-12 large-6 columns">
+									<p><span class="brand">The Yarn Basket</span> is owned and run by Susan Yeager.  Feel free to stop by and say "hi!".</p>
+									<ul class="small-block-grid-1">
+										<li><img class="th" src="http://placekitten.com/600/500" /></li>
+									</ul>
+								</div>
+							</div>
+							<!-- end about us section row -->
+							<!-- start directions section row -->
+							<div class="row">
+								<div class="small-12 columns">
+									<h1 id="contact-us">Contact Us</h1>
+								</div>
+								<!-- start direcitons section left -->
+								<div class="small-12 medium-12 large-4 columns">
+									<div class="stitched">
+										<h3>Phone</h3>
+										<p>(717)263-3236</p>
+									</div>
+									<div class="stitched">
+										<h3>Address</h3>
+										<address>150 Falling Springs Rd.<br/>Chambersburg, PA 17202</address>
+									</div>
+								</div>
+								<!-- end direcitons section left -->
+								<!-- start directions section right -->
+								<div class="small-12 medium-12 large-8 columns">	
+									<ul class="small-block-grid-1">
+										<li><img class="th" src="http://placehold.it/600x400&text=google+map" /></li>
+									</ul>
+								</div>
+								<!-- end directions section right -->
+								<div class="small-12 columns">
+									<p>We are located at the intersection of I-81, exit #16, & PA route #30.</p>
+									<p>If you are coming from the south, take exit #16 off I-81.  At the bottom of the exit ramp you will make a right. At the very next traffic light (Falling Spring Rd) make a right. We are located 1/8 mi. on right.</p>
+									<p>If you are coming from the north, take exit #16 off I-81.  At the bottom of the exit ramp make a left.  At the second traffic light (Falling Spring Rd) make a right. We are located 1/8 mile on the right.</p>
+								</div>
+							</div>
+							<!-- end directions section row -->
+							
+							<!-- start products section row -->
+							<div class="row">
+								<div class="small-12 columns">
+									<h1 id="products">Products</h1>
+								</div>
+								<div class="small-12 medium-12 large-6 columns">
+									<p><span class="brand">The Yarn Basket</span> is your one-stop shop for needlework supplies!</p>
+									<ul>
+										<li>Colinette Afghan Kits</li>
+										<li>Knitter's Pride Needles</li>
+										<li>Fabric Scarf - Tecido Trico</li>
+										<li>Summer Yarns</li>
+										<li>Knitting Bags</li>
+										<li>Circular Kneedle Holders</li>
+										<li>Della Q Bags</li>
+										<li>Noro Sock Yarn</li>
+										<li>Buttons</li>
+										<li>Zitron Savanna</li>
+										<li>Debbie Bliss</li>
+										<li>Donegal Luxury Tweed</li>
+										<li>Daylily</li>
+									</ul>
+								</div>
+								<div class="small-12 medium-12 large-6 columns">
+									<ul class="small-block-grid-1">
+										<li><img class="th" src="http://placekitten.com/320/300" /></li>
+										<li><img class="th" src="http://placekitten.com/320/280" /></li>
+									</ul>
+								</div>
+							</div>
+							<!-- end products section row -->
+							<!-- start classes section row -->
+							<div class="row">
+								<div class="small-12 columns">
+									<h1 id="classes">Classes</h1>
+								</div>
+								<div class="small-12 medium-12 large-6 columns">
+									<p>Looking to improve your needleworks skills and want to have a great time doing so? From beginners to experts, we have a class for you.</p>
+									<div class="stitched">
+										<p>For more information, call <span class="brand">The Yarn Basket</span> at <strong>(717)263-3236</strong>.</p>
+									</div>
+								</div>
+								<div class="small-12 medium-12 large-6 columns">
+									<ul class="small-block-grid-1 medium-block-grid-2">
+										<li><img class="th" src="http://placekitten.com/320/420" /></li>
+										<li><img class="th" src="http://placekitten.com/300/400" /></li>
+										<li><img class="th" src="http://placekitten.com/340/440" /></li>
+										<li><img class="th" src="http://placekitten.com/300/400" /></li>
+									</ul>
+								</div>
+							</div>
+							<!-- end classes section row -->
+							<!-- start sale section row -->
+							<div class="row">
+								<div class="small-12 columns">
+									<h1 id="sale">Sale</h1>
+								</div>
+								<div class="small-12 medium-12 large-6 columns">
+									<p>Yarn & Knitting Bags 25% Off</p>
+									<p>Discontinued Yarn 50% Off</p>
+									<p>Ruffled Scarf Yarn $2.00 Each</p>
+									<p>*No discounts on special orders. No returns on merchandise.*</p>
+								</div>
+								<div class="small-12 medium-12 large-6 columns">
+									<ul class="small-block-grid-1 medium-block-grid-2">
+										<li><img class="th" src="http://placekitten.com/400/300" /></li>
+										<li><img class="th" src="http://placekitten.com/400/300" /></li>
+										<li><img class="th" src="http://placekitten.com/400/300" /></li>
+										<li><img class="th" src="http://placekitten.com/400/300" /></li>
+									</ul>
+								</div>
+							</div>
+							<!-- end sale section row -->
+						</div>
+						<!-- end main page content -->
+					</div>
+					<!-- end sidebar and main page content -->
+					<!-- start footer content -->
+					<footer class="row">
+						<div class="small-12 columns">
+							<p>&copy <span class="brand">The Yarn Basket</span> &mdash; Designed by Savanah Steigen</p>
+						</div>
+					</footer>
+					<!-- end footer content -->
+				</div>
+			</div>
+			<!-- end page wrapper row-->
+		</div>
+
+	<script src="js/vendor/jquery.js"></script>
+  	<script src="js/foundation.min.js"></script>
+  	<script>
+    	$(document).foundation();
+  	</script>
+</body>
+</html>
